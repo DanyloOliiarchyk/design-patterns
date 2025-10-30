4de2aace7053168304c0a8437281aa58f0aeb3fd
danylooliiarchyk <oliyarchyk99@gmail.com>
2025-10-30 18:57:36 +0200

    Interpreter Pattern extension

---
+++ src/main/java/com/oliiarchyk/designpatterns/interpreter/impl/MultiplyExpression.java
@@ -0,0 +1,19 @@
+package com.oliiarchyk.designpatterns.interpreter.impl;
+
+import com.oliiarchyk.designpatterns.interpreter.Expression;
+
+public class MultiplyExpression implements Expression {
+
+    private final Expression left;
+    private final Expression right;
+
+    public MultiplyExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int interpret() {
+        return left.interpret() * right.interpret();
+    }
+}
--- src/test/java/com/oliiarchyk/designpatterns/interpreter/ExpressionTest.java
+++ src/test/java/com/oliiarchyk/designpatterns/interpreter/ExpressionTest.java
@@ -1,6 +1,7 @@
 package com.oliiarchyk.designpatterns.interpreter;
 
 import com.oliiarchyk.designpatterns.interpreter.impl.AddExpression;
+import com.oliiarchyk.designpatterns.interpreter.impl.MultiplyExpression;
 import com.oliiarchyk.designpatterns.interpreter.impl.NumberExpression;
 import com.oliiarchyk.designpatterns.interpreter.impl.SubtractExpression;
 import org.junit.jupiter.api.Test;
@@ -12,17 +13,17 @@ class ExpressionTest {
     @Test
     void interpreterTest() {
         // Equivalent to: 5 + 10 - 2
-        var expression = new SubtractExpression(
+        var expression = new MultiplyExpression(new SubtractExpression(
                 new AddExpression(
                         new NumberExpression(5),
                         new NumberExpression(10)
                 ),
                 new NumberExpression(2)
-        );
+        ), new NumberExpression(10));
 
         int result = expression.interpret();
 
-        assertEquals(13, result);
+        assertEquals(130, result);
         System.out.println("Result: " + result);
     }
 
